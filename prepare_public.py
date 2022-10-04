3bffa95e6b9cb6d4ac1c40bf3628585a8aef74bb
Marcel Haas <m.r.haas@uva.nl>
2022-10-04 09:51:20 +0200

    new data

--- prepare_public.py
+++ prepare_public.py
@@ -3,7 +3,7 @@ from docenten import hash_nr, preprocess
 
 datapath = "/home/marcel/projects_local/Laura/Docentenbeleid/Data/"
 
-df_raw = pd.read_excel(datapath + "Docenten_2020-2022_220908.xlsx")
+df_raw = pd.read_excel(datapath + "Docenten_2020-2022_221004.xlsx")
 df = preprocess(df_raw)
 
 df = hash_nr(df, "persnr")
